24a6e1ca999fc136cc5bb0d5dc3b77265fd95ce4
RobDab <roberto.dabb@gmail.com>
2022-10-28 17:55:33 +0200

    Logic Implemented
    Still need to associate HTML and implement UI/UX

--- js/script.js
+++ js/script.js
@@ -1,1 +1,53 @@
 "use strict";
+var Recharge;
+(function (Recharge) {
+    Recharge[Recharge["S"] = 5] = "S";
+    Recharge[Recharge["M"] = 10] = "M";
+    Recharge[Recharge["L"] = 15] = "L";
+    Recharge[Recharge["Xl"] = 20] = "Xl";
+})(Recharge || (Recharge = {}));
+// classe smartphone che importa l'interfaccia
+class smartphone {
+    constructor(marca, modello) {
+        this.marca = marca;
+        this.modello = modello;
+        // marca: string;
+        // modello: string;
+        this.credit = 0;
+        this.calls = 0;
+        this.marca = marca;
+        this.modello = modello;
+    }
+}
+smartphone.callCounter = 0;
+let Galaxy = new smartphone('Samsung', 'Galaxy');
+// classe abastract User 
+// un User astratto avrà tra i prorpi attributi anche uno smartphone (rapporto di "associazione/appartenenza")
+// => dallo user1 o 2 potrò accedere ai parametri (chiamate e credito ad esempio) dei rispettivi telefoni.
+// avranno un metodo per ricaricare il credito del proprio smartphone.
+class User {
+    constructor(name, smartphone) {
+        this.name = name;
+        this.smartphone = smartphone;
+        this.name = name;
+        this.smartphone = smartphone;
+    }
+    buyCredit(cash) {
+        this.smartphone.credit += cash;
+        console.log(`Nuovo credito: ${this.smartphone.credit}€`);
+    }
+    //tarffa = 0,20 cent/minuto
+    call(duration) {
+        if (this.smartphone.credit / 0.20 >= duration) {
+            this.smartphone.calls += 1;
+            this.smartphone.credit -= duration * 0.20;
+            smartphone.callCounter += 1;
+            console.log(`credito residuo: ${this.smartphone.credit.toFixed(2)}`);
+        }
+        else
+            (console.log('credito insufficiente, ricarica. usando "IlTuoNome".buyCredit(Recharge.[S/M/L/XL])'));
+    }
+}
+let Mario = new User('Mario', new smartphone('Samsung', 'Galaxy'));
+let Bobbie = new User('Bobbie', new smartphone('Redmi', 'Note'));
+let Will = new User('Will', new smartphone('Apple', 'IPhone'));
---
+++ ts/script.ts
@@ -0,0 +1,68 @@
+// Interfaccia smartphone
+interface Phone {
+    marca: string;
+    modello: string;
+    credit: number;
+    calls: number;
+}
+
+
+enum Recharge {         //Enum usato per impostare delle soglie di ricarica predefinite.
+    S = 5,
+    M = 10,
+    L = 15,
+    Xl = 20,
+}
+
+
+// classe smartphone che importa l'interfaccia
+class smartphone implements Phone {
+    // marca: string;
+    // modello: string;
+    credit: number = 0;
+    calls: number = 0;
+    static callCounter = 0;
+    constructor(public marca:string, public modello:string){
+        this.marca = marca; 
+        this.modello = modello;
+    }
+}
+
+
+
+let Galaxy = new smartphone('Samsung','Galaxy')
+// classe abastract User 
+// un User astratto avrà tra i prorpi attributi anche uno smartphone (rapporto di "associazione/appartenenza")
+// => dallo user1 o 2 potrò accedere ai parametri (chiamate e credito ad esempio) dei rispettivi telefoni.
+// avranno un metodo per ricaricare il credito del proprio smartphone.
+class User {
+    
+    constructor(public name: string,public smartphone: smartphone){
+        this.name = name;
+        this.smartphone = smartphone;
+    }
+
+    buyCredit(cash: Recharge): void {
+        this.smartphone.credit += cash;
+        console.log(`Nuovo credito: ${this.smartphone.credit}€`)
+    }
+
+        //tarffa = 0,20 cent/minuto
+    call(duration: number){
+        if(this.smartphone.credit/0.20 >= duration){
+            this.smartphone.calls += 1;
+            this.smartphone.credit -= duration * 0.20;
+            smartphone.callCounter += 1;
+            console.log(`credito residuo: ${this.smartphone.credit.toFixed(2)}`)
+        }else (
+            console.log('credito insufficiente, ricarica. usando "IlTuoNome".buyCredit(Recharge.[S/M/L/XL])')
+        )    
+    }
+}
+
+let Mario = new User('Mario', new smartphone('Samsung','Galaxy'))
+let Bobbie = new User('Bobbie', new smartphone('Redmi', 'Note'))
+let Will = new User('Will', new smartphone('Apple','IPhone'))
+
+
+
