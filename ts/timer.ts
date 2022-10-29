2374bbcc9a8bdceda68f48ef8d8ba70b1f3eb4d7
RobDab <roberto.dabb@gmail.com>
2022-10-29 17:58:04 +0200

    implemented rudimental Timer
    To finish and implement in the project associated with the call method of the smartphones

---
+++ ts/timer.ts
@@ -0,0 +1,20 @@
+let date1: Date;
+let date2: Date;
+
+// !BISONGA CORREGGERE LA FUNZIONE
+//! Devo creare un 'myInterval' ad esempio, che richiama la callback di timer, e imposta l'intervallo
+//! come secondo parametro. 
+//! Potrò quindi richiamare myInterval come parametro di clearInterval('myInterval) per fermare l'esecuzione del Timer
+
+
+function startTimer(){
+    let date1 = new Date;
+    setInterval(()=>{
+        let date2 = new Date;
+        console.log(Math.abs(date1.getSeconds()-date2.getSeconds()) + ' seconds passed');
+    },1000)
+}
+
+startTimer()
+
+// in una seguente funzione passata in onClick ad un button, potro inserire clearInterval
